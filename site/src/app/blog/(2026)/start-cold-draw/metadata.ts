74398f83a0f75755d23d1aa1e4ef6b52550d12f4
Tony Cerqui <adpcerqui@gmail.com>
2026-03-04 20:23:05 +1000

    post

---
+++ site/src/app/blog/(2026)/start-cold-draw/metadata.ts
@@ -0,0 +1,13 @@
+import { PostMetadata  } from '../../_types';
+
+export const blogPostMetadata: PostMetadata = {
+  title: 'Cold Draw',
+  date: new Date('2026-03-04'),
+  tags: ['personal-project', 'pkm', 'cold-draw', 'dinner-parties', 'social'],
+  excerpt: 'A tool to randomly draw names from a lifetime of contacts, invite them to dinner, and repeat—plus the PRM/PKM and cold_draw stack behind it.',
+  image: './graph.png',
+  imageAlt: 'Obsidian Graph',
+  readingTime: 7,
+  isFeatured: false,
+};
+
