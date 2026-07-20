0f7fe9ba43b21d1560585f330d54f4adcfeb5705
Tony Cerqui <adpcerqui@gmail.com>
2026-07-20 22:10:41 +1000

    fix build error

---
+++ site/src/app/blog/(2026)/intro-to-reqlan/page.tsx
@@ -0,0 +1,20 @@
+const tooltips = {
+    "python": ""
+
+}
+
+
+const sections = {
+    "introduction":<></>,
+    "extension":<></>,
+    "development-and-implementation": <></> // hidden in an accordion
+}
+
+
+export default function IntroToReqlanPage(): React.JSX.Element {
+  return (
+    <div>
+      
+    </div>
+  );
+}
